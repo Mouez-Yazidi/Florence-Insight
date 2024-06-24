ecf66c3d8f6285b1edb20a73f457da24ce756f90
Mouez Yazidi <56160970+Mouez-Yazidi@users.noreply.github.com>
2024-06-24 14:43:34 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st 
+from transformers import AutoModelForCausalLM
+from PIL import Image
+import requests
+from PIL import Image
+from io import BytesIO
+# Set the app title 
+st.title('My First Streamlit App') 
+# Add a welcome message 
+st.write('Welcome to my Streamlit app!') 
+
+option = st.radio("Select an option:", ('Upload an image', 'Enter an image URL'), index=None)
+
+if option == 'Upload an image':
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    # Open the uploaded image
+    image = Image.open(uploaded_file)
+    
+    # Display the uploaded image
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+elif option == 'Enter an image URL':
+    url = st.text_input("Enter the URL of the image:")
+    if url:
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+        st.image(img, caption='Downloaded Image', use_column_width=True)
+
+
+# Add a sidebar
+st.sidebar.title("Navigation")
+selection = st.sidebar.radio("Go to", ['Caption','Segmentation','Object Detection'])
+
+#model = AutoModelForCausalLM.from_pretrained("microsoft/Florence-2-base-ft", trust_remote_code=True)
