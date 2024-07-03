d30113528a4d91d91a3ab0d94fee507a0e32057c
Mouez Yazidi <56160970+Mouez-Yazidi@users.noreply.github.com>
2024-07-03 09:47:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,1 @@
-from setuptools import setup
-import os
-from setuptools.command.install import install  # Import the install class
 
-class CustomInstallCommand(install):
-    """Customized setuptools install command - installs flash-attn with --no-build-isolation."""
-    def run(self):
-        install.run(self)
-        os.system('pip install flash-attn --no-build-isolation')
-
-setup(
-    name='your_package',
-    version='0.1',
-    install_requires=[],
-    cmdclass={
-        'install': CustomInstallCommand,
-    },
-)
